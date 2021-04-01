28b0add3b3fc23aa85d057cad15fe90588c86c6d
Dzianis Sokalau <densokolov88@gmail.com>
2021-04-01 22:04:58 +0200

    change first_value with max

--- sql/n_day_retention.sql
+++ sql/n_day_retention.sql
@@ -2,7 +2,7 @@
 -- calculate retention rate as proportion of users opened the app on the day N and day 0
 SELECT cohort_date
     , day_number 
-    , users / FIRST_VALUE(users) OVER (PARTITION BY cohort_date ORDER BY day_number ASC) AS retention_rate
+    , users / MAX(users) OVER (PARTITION BY cohort_date) AS retention_rate
 FROM (
     -- for every app open calculate number of days after the first visit
     -- count distinct number of users for every cohort_date and day_number pair
@@ -10,7 +10,7 @@ FROM (
         , DATE_DIFF(date, cohort_date, DAY) AS day_number
         , COUNT(DISTINCT user_id) AS users
     FROM (
-        -- get the first app visit data for every iser
+        -- get the first app visit data for every user
         SELECT user_id
             , date
             , MIN(date) OVER (PARTITION BY user_id) AS cohort_date
