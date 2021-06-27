44f08c4b1dbcfdb2778eb31cd931a0fc09cde1f2
Dzianis Sokalau <densokolov88@gmail.com>
2021-06-27 22:39:10 +0200

    update n day retention

--- description/n_day_retention.md
+++ description/n_day_retention.md
@@ -1,8 +1,9 @@
 # N day retention  
   
-N day retention shows the percent of users who return to the app on a specified day after their first visit.  
-  
-In order to calculate N day retention, we need to follow the next steps (BigQuery syntax is used).  
+**Task**  
+We want to know what percentage of users return to our product on a specific day after their first visit. Users should be grouped by the date of the first visit. 
+
+
 1. Find the first app open date for every user. Probably the most tidy and efficient way of doing it would be a window function:  
 ```sql
 MIN(date) OVER (PARTITION BY user_id) AS cohort_date
@@ -16,7 +17,19 @@ SELECT cohort_date
 3. Now when we have number of users for every `cohort_date` and `day_number` combination, we can calculate N day retention itself:  
 ```sql
 users / MAX(users) OVER (PARTITION BY cohort_date) AS retention_rate
+```   
+4. Generating sequence of numbers. It will help to have result for each day number even if there were no users on this day. Below is Big Query variant, other data bases may have different approaches. Alternatively a table may be used.  
+```sql
+SELECT day_number FROM UNNEST(GENERATE_ARRAY(0, 364)) AS day_number
+```    
+5. Join generated day_numbers and cohort dates to get all combination of `cohort_date` and `day_number`. With this approach we should remove all future days:  
+```sql
+WHERE DATE_ADD(cohort_date, INTERVAL day_number DAY) < CURRENT_DATE
 ```  
-
+6. Finally join retention and all posible combination of `cohort_date` and `day_number` and fill missing values with 0:   
+```sql
+, COALESCE(retention_rate, 0) AS retention_rate
+```   
+  
 The full query is buy the link: https://github.com/dzianissokalau/sql_analysis/blob/main/sql/n_day_retention.sql  
 
--- sql/n_day_retention.sql
+++ sql/n_day_retention.sql
@@ -1,21 +1,43 @@
 -- calculate retention rate for every cohort_date and day_number pair
 -- calculate retention rate as proportion of users opened the app on the day N and day 0
-SELECT cohort_date
-    , day_number 
-    , users / MAX(users) OVER (PARTITION BY cohort_date) AS retention_rate
-FROM (
-    -- for every app open calculate number of days after the first visit
-    -- count distinct number of users for every cohort_date and day_number pair
+-- solving no visits days problem with sequence generator
+WITH cohorts AS (
     SELECT cohort_date
-        , DATE_DIFF(date, cohort_date, DAY) AS day_number
-        , COUNT(DISTINCT user_id) AS users
+        , day_number 
+        , users / MAX(users) OVER (PARTITION BY cohort_date) AS retention_rate
     FROM (
-        -- get the first app visit data for every user
-        SELECT user_id
-            , date
-            , MIN(date) OVER (PARTITION BY user_id) AS cohort_date
-        FROM `data-analysis-sql-309220.synthetic.app_open`
-    ) AS a0
-    GROUP BY 1,2
-) AS a1
+        -- for every app open calculate number of days after the first visit
+        -- count distinct number of users for every cohort_date and day_number pair
+        SELECT cohort_date
+            , DATE_DIFF(date, cohort_date, DAY) AS day_number
+            , COUNT(DISTINCT user_id) AS users
+        FROM (
+            -- get the first app visit data for every user
+            SELECT user_id
+                , date
+                , MIN(date) OVER (PARTITION BY user_id) AS cohort_date
+            FROM `table_with_visit_events`
+        ) AS a0
+        GROUP BY 1,2
+    ) AS a1
+)
+SELECT days.cohort_date
+    , days.day_number
+    , COALESCE(retention_rate, 0) AS retention_rate
+FROM (
+    -- generate sequence of numbers between 0 and 364 (first 365 days)
+    SELECT cohort_date
+        , day_number 
+    FROM UNNEST(GENERATE_ARRAY(0, 364)) AS day_number
+    -- join to every cohort date, so we will have retention for all days between 0 and 364
+    CROSS JOIN (
+        SELECT DISTINCT cohort_date
+        FROM cohorts
+    ) AS c0
+    -- filter out future days
+    WHERE DATE_ADD(cohort_date, INTERVAL day_number DAY) < CURRENT_DATE
+) AS days
+-- join cohorts and replace no visites days with 0 (retention rate = 0)
+LEFT JOIN cohorts AS c1 ON c1.cohort_date = days.cohort_date
+    AND c1.day_number = days.day_number
 ORDER BY 1,2;
